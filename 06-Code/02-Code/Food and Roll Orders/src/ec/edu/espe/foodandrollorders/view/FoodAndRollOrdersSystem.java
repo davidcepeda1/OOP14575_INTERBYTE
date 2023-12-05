50bd8c8b203dfb8cdd4a6c3166c73ea16e71fc96
Teo344 <gabriel2004mat@gmail.com>
2023-12-05 13:21:11 -0500

    create menu for aplication V1.3.0

--- 06-Code/02-Code/Food and Roll Orders/src/ec/edu/espe/foodandrollorders/view/FoodAndRollOrdersSystem.java
+++ 06-Code/02-Code/Food and Roll Orders/src/ec/edu/espe/foodandrollorders/view/FoodAndRollOrdersSystem.java
@@ -3,109 +3,147 @@ package ec.edu.espe.foodandrollorders.view;
 import java.util.Scanner;
 
 public class FoodAndRollOrdersSystem {
-    public static void main(String[] args) {
-        try (Scanner scan = new Scanner(System.in)) {
-            int option;
-            header();
-            do {
-                
-                while (!scan.hasNextInt()) {
-                    System.out.println("Error: Ingresa un numero entero valido.");
-                    scan.next();
-                }
-                
-                option = scan.nextInt();
-                
-                switch (option) {
-                    case 1 -> {
-                        user();
-                        header();
-                    }
-                    
-                    case 2 -> {
-                        customer();
-                        header();
-                    }
-                    
-                    case 3 -> {
-                        System.out.println("Exiting...");
-                        break;
-                    }
-                    
-                    default -> System.out.println("Invalid option. Please select a valid option.");
-                }
-            } while (option != 3);
-        }
-    }
 
-    
-    public static void customer(){
-        try (Scanner scan = new Scanner(System.in)) {
-            int option;
-            header();
-            do {
-                
-                while (!scan.hasNextInt()) {
-                    System.out.println("Error: Ingresa un numero entero valido.");
-                    scan.next();
-                }
-                
+    public static void main(String[] args) {
+        int i=1;
+        do{
+        System.out.println("********************");
+        System.out.println("Bienvenido al restaurante food and roll");
+        
+        int option = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("Que tipo de persona es?");
+            System.out.println("1. Usuario");
+            System.out.println("2. Cliente");
+            System.out.println("Seleccione una opcion");
+
+            try {
                 option = scan.nextInt();
-                
-                switch (option) {
-                    case 1 -> {
-                        user();
-                        header();
-                    }
-                    
-                    case 2 -> {
-                        customer();
-                        header();
-                    }
-                    
-                    case 3 -> {
-                        System.out.println("Exiting...");
-                        break;
-                    }
-                    
-                    default -> System.out.println("Invalid option. Please select a valid option.");
+
+                if (option != 1 && option != 2) {
+                    System.out.println("Opcion no valida. Intentelo de nuevo.");
                 }
-            } while (option != 3);
+
+            } catch (Exception e) {
+                System.out.println("Ingrese solo numeros");
+                scan.nextLine();
+            }
+        } while (option != 1 && option != 2);
+
+        switch (option) {
+            case 1:
+                enterUser();
+                break;
+                
+            case 2:
+                enterCustomer();
+                break;
+                
+            default:
+                throw new AssertionError();
         }
-    }
-    
-    
-    
-    
-    public static void user(){
+        }while(i==1);
         
     }
     
     
+    public static void enterUser () {
+        int option = 0;
+        do{
+        do {
+            System.out.println("****************");
+            System.out.println("User");
+            System.out.println("Que desea hacer");
+            System.out.println("1.Crear Usuario");
+            System.out.println("2.Ingresar usuario");
+            System.out.println("3 Regresar");
+            System.out.println("Seleccione una opcion");
+            Scanner scan = new Scanner(System.in);
+            
+            try {
+                option = scan.nextInt();
+
+                if (option != 1 && option != 2 && option!=3) {
+                    System.out.println("Opcion no valida. Intentelo de nuevo.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Ingrese solo numeros");
+                scan.nextLine();
+            }
+        } while (option != 1 && option != 2 && option !=3);
+
+        switch (option) {
+            case 1:
+                System.out.println("Caso 1 de usurio");
+                break;
+                
+            case 2:
+                System.out.println("caso 2 usuario");
+                break;
+            
+            case 3 :
+                System.out.println("Regresando...");
+                break;
+                
+            default:
+                throw new AssertionError();
+        }
+    }while(option != 3);
+        }
     
-    
-    
-    
-    
-    public static void header() {
-        System.out.println("--------------------------");
-        System.out.println("----------Wecolme---------");
-        System.out.println("-------Food and Roll------");
-        System.out.println("--------------------------");
-        System.out.println("---------1. User----------");
-        System.out.println("---------2. Orderds-------");
-        System.out.println("---------3. Exit----------");
-        System.out.println("--------------------------");
-        System.out.println("-----Selecciona una opcion-----\n");
+
+
+
+    public static void enterCustomer(){
+        int option = 0;
+        do{
+        do {
+            System.out.println("****************");
+            System.out.println("Customer");
+            System.out.println("Que desea hacer");
+            System.out.println("1.Ver menu");
+            System.out.println("2.Realizar Pedido");
+            System.out.println("3.Ver pedido");
+            System.out.println("4.Regresar");
+            System.out.println("Seleccione una opcion");
+            Scanner scan = new Scanner(System.in);
+            
+            try {
+                option = scan.nextInt();
+
+                if (option != 1 && option != 2 && option!=3 && option !=4) {
+                    System.out.println("Opcion no valida. Intentelo de nuevo.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Ingrese solo numeros");
+                scan.nextLine();
+            }
+        } while (option != 1 && option != 2 && option !=3 && option !=4);
+
+        switch (option) {
+            case 1:
+                System.out.println("Caso 1 ver menu");
+                break;
+                
+            case 2:
+                System.out.println("caso 2 realizar pedido");
+                break;
+            
+            case 3 :
+                System.out.println("caso 3 ver pedido");
+                break;
+                
+            case 4 :
+                System.out.println("Regresando....");
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }while(option != 4);
     }
-    
-    public static void headerUser(){
-        System.out.println("--------------------------");
-        System.out.println("-----User-----\n");
-        System.out.println("--------------------------");
-        System.out.println("-----User-----\n");
-    }
-    
-    
-    
 }
