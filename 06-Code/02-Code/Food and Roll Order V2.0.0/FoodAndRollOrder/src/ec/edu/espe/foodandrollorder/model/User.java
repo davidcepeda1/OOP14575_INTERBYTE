f1214b20f05c1cecf83ef0f2ccae3eb4bcb9ba9b
Teo344 <gabriel2004mat@gmail.com>
2023-12-09 16:13:37 -0500

    create class userId V2.1.0

--- 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/model/User.java
+++ 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/model/User.java
@@ -1,13 +1,87 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espe.foodandrollorder.model;
+
+import java.util.Date;
 
 /**
  *
  * @author RC_558
  */
 public class User {
+    private String userId;
+    private String password;
+    private String loginStatus;
+    private Date registerDate;
+
+    public User(String userId, String password, String loginStatus, Date registerDate) {
+        this.userId = userId;
+        this.password = password;
+        this.loginStatus = loginStatus;
+        this.registerDate = registerDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userId=" + getUserId() + ", password=" + getPassword() + ", loginStatus=" + getLoginStatus() + ", registerDate=" + getRegisterDate() + '}';
+    }
+
+    /**
+     * @return the userId
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the loginStatus
+     */
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    /**
+     * @param loginStatus the loginStatus to set
+     */
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    /**
+     * @return the registerDate
+     */
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    /**
+     * @param registerDate the registerDate to set
+     */
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
     
+    public static void verifyLogin(){
+        
+    }
 }
