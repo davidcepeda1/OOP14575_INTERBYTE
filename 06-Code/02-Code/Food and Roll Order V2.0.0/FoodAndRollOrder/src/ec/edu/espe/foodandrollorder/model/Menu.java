5e4a6b184ff66115213247354a52af564ce542fa
Teo344 <gabriel2004mat@gmail.com>
2023-12-09 16:54:52 -0500

    Update class RestaurantInformation-Menu-User V2.3.0

--- 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/model/Menu.java
+++ 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/model/Menu.java
@@ -1,5 +1,32 @@
 package ec.edu.espe.foodandrollorder.model;
 
+import java.util.ArrayList;
+
 public class Menu {
+    private ArrayList <Plate> plateList;
+
+    public Menu(ArrayList<Plate> plateList) {
+        this.plateList = plateList;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" + "plateList=" + getPlateList() + '}';
+    }
+
+    /**
+     * @return the plateList
+     */
+    public ArrayList <Plate> getPlateList() {
+        return plateList;
+    }
+
+    /**
+     * @param plateList the plateList to set
+     */
+    public void setPlateList(ArrayList <Plate> plateList) {
+        this.plateList = plateList;
+    }
+    
     
 }
--- 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/view/FoodAndRollSystem.java
+++ 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/view/FoodAndRollSystem.java
@@ -1,9 +1,92 @@
 
 package ec.edu.espe.foodandrollorder.view;
 
+import ec.edu.espe.foodandrollorder.model.RestaurantInformation;
+import java.util.Scanner;
+
 public class FoodAndRollSystem {
     
     public static void main(String[] args) {
+        int option = 0;
+        registerManager();
+        while(option !=4){
+        RestaurantInformation restaurantInfo = new RestaurantInformation(
+                "123 Main St",
+                "555-1234",
+                "info@example.com",
+                "Next to the Park");
+        restaurantInfo.showSpecificLocation();
+        printUser();
+        option=validateOptionCenter(option);
+        
+            switch (option) {
+                case 1:
+                    manager();
+                    break;
+                case 2:
+                    chef();
+                    break; 
+                case 3:
+                    customer();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");;
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+    }
+    
+    
+    
+    
+    private static int validateOptionCenter(int option){
+            Scanner scann = new Scanner(System.in);
+            do{
+                System.out.println("Select to Option");
+                
+        try {
+                option=scann.nextInt();
+                if (option != 1 && option != 2 && option !=3 && option !=4) {
+                    System.out.println("Opcion no valida. Intentelo de nuevo.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Ingrese solo numeros");
+                scann.nextLine();
+            }
+        } while (option != 1 && option != 2 && option !=3 && option !=4);
+        return option;
+    }
+    
+    public static void printUser(){
+        System.out.println("1. Manager");
+        System.out.println("2. Chef");
+        System.out.println("3. Customer");
+        System.out.println("4. Exit");
+        System.out.println("*****************************");
+    }
+    
+    public static void registerManager(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("*************");
+        System.out.println("Inicializar con el sistema");
+        System.out.println("registre el usuario para el manager");
         
     }
+    
+    public static void manager(){
+        
+    }
+    
+    public static void chef(){
+        
+    }
+    
+    public static void customer(){
+        
+    }
+    
 }
+
