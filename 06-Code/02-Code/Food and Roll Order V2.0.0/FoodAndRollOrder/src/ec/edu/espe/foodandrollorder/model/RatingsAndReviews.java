cfe4b3d248168ac0b7507fb22cee5cc7ff846c2e
Cepeda Salguero David Gustavo <150314536+davidcepeda1@users.noreply.github.com>
2023-12-09 17:30:36 -0500

    Class  RatingReviews, Reviews and Manager added with constructor, to string, and encapsulate fields

--- 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/model/RatingsAndReviews.java
+++ 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/model/RatingsAndReviews.java
@@ -1,13 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ec.edu.espe.foodandrollorder.model;
+
+import java.util.Date;
 
 /**
  *
  * @author RC_558
  */
 public class RatingsAndReviews {
+    private int commentId;
+    private Date commentDate;
+    private String contentComments;
+
+    public RatingsAndReviews(int commentId, Date commentDate, String contentComments) {
+        this.commentId = commentId;
+        this.commentDate = commentDate;
+        this.contentComments = contentComments;
+    }
+
+    @Override
+    public String toString() {
+        return "RatingsAndReviews{" + "commentId=" + commentId + ", commentDate=" + commentDate + ", contentComments=" + contentComments + '}';
+    }
     
+    public void postComment(){
+        
+    }
+
+    public void removeComment(){
+        
+    }
+    
+    public void addComment(){
+        
+    }
+    
+    public void addRating(){
+        
+    }
+    
+    public void removeRating(){
+        
+    }
+    
+    /**
+     * @return the commentId
+     */
+    public int getCommentId() {
+        return commentId;
+    }
+
+    /**
+     * @param commentId the commentId to set
+     */
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    /**
+     * @return the commentDate
+     */
+    public Date getCommentDate() {
+        return commentDate;
+    }
+
+    /**
+     * @param commentDate the commentDate to set
+     */
+    public void setCommentDate(Date commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    /**
+     * @return the contentComments
+     */
+    public String getContentComments() {
+        return contentComments;
+    }
+
+    /**
+     * @param contentComments the contentComments to set
+     */
+    public void setContentComments(String contentComments) {
+        this.contentComments = contentComments;
+    }
+
+    
+
 }
