6883a9ed6489d7cf24daa88270c0193bc45f8112
SEBASTIAN_CRIOLLO <sebastiancriollo943@gmail.com>
2023-12-10 16:04:26 -0500

    Create GrilledSpicy

---
+++ 06-Code/02-Code/Food and Roll Order V2.0.0/FoodAndRollOrder/src/ec/edu/espe/foodandrollorder/model/GrilledShrimp.java
@@ -0,0 +1,28 @@
+
+package ec.edu.espe.foodandrollorder.model;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class GrilledShrimp extends Plate{
+    
+    private boolean spicy;
+    
+    public GrilledShrimp(int id,String name,String description, double price,String availability,double preparationTime,boolean spicy){
+        super(id,name,"Seafood",description,price,availability,preparationTime);
+        this.spicy = spicy;
+    }
+    public void getDails(){
+        super.getDetails();
+        System.out.println("Spicy"+spicy);
+    }
+    
+    public boolean isSpicy(){
+        return spicy;
+    }
+    
+    public void setSpicy(boolean spicy){
+        this.spicy =spicy;
+    }
+}
